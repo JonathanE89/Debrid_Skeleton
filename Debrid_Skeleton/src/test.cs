caf9da77364f87320ea420276dd100ae35892e22
AlicesVoid <hypnocarrot@csu.fullerton.edu>
2023-04-25 15:53:52 -0700

    adding src folder
    adding src folder

---
+++ Debrid_Skeleton/src/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debrid_Skeleton.src
+{
+    internal class test
+    {
+    }
+}
