8609cb30d24aa7a22df25ff1984305096dbb905b
Alice Void <hypnocarrot@csu.fullerton.edu>
2023-04-28 22:19:06 -0700

    AccountUI is Complete- Missing UX/Logic Components

--- pythonCloudDebrid/accountUI.py
+++ pythonCloudDebrid/accountUI.py
@@ -97,8 +97,8 @@ class LoginWindow(QMainWindow):
         
         #Method to show the RegisterWindow
         def show_register_window():
-            self.register_window.show()
-                
+            self.register_window.show() 
+                          
         #Establish Layout of User Info
         info_layout = QVBoxLayout()
         info_layout.setSpacing(1)
@@ -160,6 +160,83 @@ class LoginWindow(QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
+class AccountWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Account")
+        window_width = 400
+        window_height = 200
+        self.setFixedSize(window_width, window_height)
+        layout = QVBoxLayout()
+        
+        # Create instances of LoginWindow and RegisterWindow
+        self.login_window = LoginWindow()
+        self.register_window = RegisterWindow()
+
+        # Establish Layout of User Info
+        info_layout = QVBoxLayout()
+        info_layout.setSpacing(1)
+        info_layout.setContentsMargins(0, 0, 0, 10)
+
+        # Add a label for the username
+        self.username_label = QLabel("Username:")
+        info_layout.addWidget(self.username_label)
+
+        # Add a text box for the username
+        self.username_box = QLineEdit("No User Detected")
+        self.username_box.setReadOnly(True)
+        info_layout.addWidget(self.username_box)
+
+        # Add a label for the password
+        #self.password_label = QLabel("Password:")
+        #info_layout.addWidget(self.password_label)
+
+        # Add a text box for the password
+        #self.password_box = QLineEdit("No User Detected")
+        #self.password_box.setEchoMode(QLineEdit.EchoMode.Password)
+        #self.password_box.setReadOnly(True)
+        #info_layout.addWidget(self.password_box)
+
+        # Incorporates the info_layout into the main layout
+        layout.addLayout(info_layout)
+
+        # Add horizontal frame separator
+        separator = QFrame()
+        separator.setFrameShape(QFrame.HLine)
+        separator.setFrameShadow(QFrame.Sunken)
+        layout.addWidget(separator)
+
+        # Establish Layout for Buttons
+        button_layout = QHBoxLayout()
+
+        # Add button 1 (Log In)
+        button_1 = QPushButton("Log In")
+        button_layout.addWidget(button_1)
+        button_1.clicked.connect(self.show_login_window)
+
+        # Add spacer item to evenly distribute buttons
+        button_layout.addStretch()
+
+        # Add button 2 (Sign Up)
+        button_2 = QPushButton("Sign Up")
+        button_layout.addWidget(button_2)
+        button_2.clicked.connect(self.show_register_window)
+
+        # Add button layout to main layout
+        layout.addLayout(button_layout)
+
+        # Set the layout for the central widget
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def show_login_window(self):
+        self.login_window.show()
+
+    def show_register_window(self):
+        self.register_window.show()
+
+
 #Launch Windows
 '''
 def launch_windows():
@@ -180,13 +257,13 @@ if __name__ == "__main__":
 '''
 
 #Test First Window
-def launch_LoginWindow():      
+def launch_AccountWindow():      
     if __name__ == "__main__":
         app = QApplication([])
-        window = LoginWindow()
+        window = AccountWindow()
         window.show()
         app.exec()
 
 #Actual Executed Code 
-launch_LoginWindow()
+launch_AccountWindow()
 
